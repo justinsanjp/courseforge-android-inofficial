7193495d0bf210057c9db98e465fabf4e6de6b73
justinweichTV <64624657+justinweichTV@users.noreply.github.com>
2023-01-30 09:36:08 +0100

    Add files via upload

---
+++ app/src/main/java/com/overwolf/courseforge/HomeActivity.java
@@ -0,0 +1,210 @@
+package com.overwolf.courseforge;
+
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.*;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class HomeActivity extends Activity {
+	
+	private Timer _timer = new Timer();
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private WebView webview1;
+	private ImageView imageview1;
+	private LinearLayout linear3;
+	private Button button1;
+	private ImageView imageview2;
+	private LinearLayout linear4;
+	private ImageView imageview3;
+	
+	private RequestNetwork request;
+	private RequestNetwork.RequestListener _request_request_listener;
+	private Intent intent = new Intent();
+	private TimerTask timer;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.home);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		linear2 = findViewById(R.id.linear2);
+		webview1 = findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		imageview1 = findViewById(R.id.imageview1);
+		linear3 = findViewById(R.id.linear3);
+		button1 = findViewById(R.id.button1);
+		imageview2 = findViewById(R.id.imageview2);
+		linear4 = findViewById(R.id.linear4);
+		imageview3 = findViewById(R.id.imageview3);
+		request = new RequestNetwork(this);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				
+			}
+		});
+		
+		imageview2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				webview1.loadUrl("https://www.curseforge.com/minecraft/mc-mods");
+			}
+		});
+		
+		imageview3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				webview1.loadUrl("https://www.curseforge.com/wow/addons");
+			}
+		});
+		
+		_request_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2, HashMap<String, Object> _param3) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				final HashMap<String, Object> _responseHeaders = _param3;
+				
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				intent.setClass(getApplicationContext(), OfflineActivity.class);
+				startActivity(intent);
+			}
+		};
+	}
+	
+	private void initializeLogic() {
+		webview1.loadUrl("https://www.curseforge.com/");
+		for(int _repeat77 = 0; _repeat77 < (int)(999); _repeat77++) {
+			timer = new TimerTask() {
+				@Override
+				public void run() {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							request.startRequestNetwork(RequestNetworkController.GET, "https://www.curseforge.com/", "", _request_request_listener);
+						}
+					});
+				}
+			};
+			_timer.schedule(timer, (int)(5000));
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		webview1.goBack();
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
---
+++ app/src/main/java/com/overwolf/courseforge/MainActivity.java
@@ -0,0 +1,181 @@
+package com.overwolf.courseforge;
+
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.*;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends Activity {
+	
+	private Timer _timer = new Timer();
+	
+	private LinearLayout linear2;
+	private LinearLayout linear3;
+	private LinearLayout linear4;
+	private LinearLayout linear5;
+	private LinearLayout linear6;
+	private LinearLayout linear7;
+	private ImageView imageview1;
+	private LinearLayout linear8;
+	private LinearLayout linear9;
+	private TextView textview1;
+	
+	private RequestNetwork network;
+	private RequestNetwork.RequestListener _network_request_listener;
+	private AlertDialog.Builder dialog;
+	private TimerTask time;
+	private Intent n = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear2 = findViewById(R.id.linear2);
+		linear3 = findViewById(R.id.linear3);
+		linear4 = findViewById(R.id.linear4);
+		linear5 = findViewById(R.id.linear5);
+		linear6 = findViewById(R.id.linear6);
+		linear7 = findViewById(R.id.linear7);
+		imageview1 = findViewById(R.id.imageview1);
+		linear8 = findViewById(R.id.linear8);
+		linear9 = findViewById(R.id.linear9);
+		textview1 = findViewById(R.id.textview1);
+		network = new RequestNetwork(this);
+		dialog = new AlertDialog.Builder(this);
+		
+		_network_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2, HashMap<String, Object> _param3) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				final HashMap<String, Object> _responseHeaders = _param3;
+				n.setClass(getApplicationContext(), HomeActivity.class);
+				startActivity(n);
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				dialog.setTitle("Conaction faild");
+				dialog.setMessage(_message);
+				dialog.setPositiveButton("retry ", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						network.startRequestNetwork(RequestNetworkController.GET, "https://www.curseforge.com/", "", _network_request_listener);
+					}
+				});
+				dialog.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						finishAffinity();
+					}
+				});
+				dialog.create().show();
+			}
+		};
+	}
+	
+	private void initializeLogic() {
+		time = new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						network.startRequestNetwork(RequestNetworkController.GET, "https://www.curseforge.com/", "", _network_request_listener);
+					}
+				});
+			}
+		};
+		_timer.schedule(time, (int)(5000));
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
